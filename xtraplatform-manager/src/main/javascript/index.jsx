a3371f5e39ff1ebcaa0deee61666ecc819f4ead4
azahnen <zahnen@interactive-instruments.de>
2017-10-05 11:16:59 +0200

    move manager to separate repository

--- xtraplatform-manager/src/main/javascript/index.jsx
+++
@@ -1,5 +0,0 @@
-
-import { app, render } from './module.js'
-import './scss/default/index'
-
-render(app);
--- xtraplatform-manager/src/main/javascript/reducers/index.js
+++
@@ -1,6 +0,0 @@
-
-import app from './app';
-import service from './service';
-
-export { service as service }
-export { app as app }
