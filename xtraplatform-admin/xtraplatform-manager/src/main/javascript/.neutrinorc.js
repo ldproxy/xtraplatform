efe70239c7320d4c0d649e402618a1091e3d21cb
azahnen <zahnen@interactive-instruments.de>
2020-03-18 16:06:02 +0100

    revert to yarn 1.x

--- xtraplatform-admin/xtraplatform-manager/src/main/javascript/.neutrinorc.js
+++ xtraplatform-admin/xtraplatform-manager/src/main/javascript/.neutrinorc.js
@@ -1,7 +1,6 @@
 const airbnb = require('@neutrinojs/airbnb');
 const reactComponents = require('@neutrinojs/react-components');
 const mocha = require('@neutrinojs/mocha');
-const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
 
 module.exports = {
   options: {
@@ -11,9 +10,5 @@ module.exports = {
     airbnb(),
     reactComponents(),
     mocha(),
-    (neutrino) => {
-      neutrino.config.resolve.plugin('pnp').use(PnpWebpackPlugin)
-      neutrino.config.resolveLoader.plugin('pnp').use(PnpWebpackPlugin.moduleLoader(module))
-    },
   ],
 };
