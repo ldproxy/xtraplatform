ce915b84b43c0d3fe060cdf111073341934365dd
azahnen <zahnen@interactive-instruments.de>
2020-08-20 16:34:39 +0200

    restructure js modules

---
+++ xtraplatform-core/xtraplatform-manager/src/main/javascript/core/.neutrinorc.js
@@ -0,0 +1,11 @@
+const airbnb = require('@neutrinojs/airbnb');
+//const reactComponents = require('@neutrinojs/react-components');
+const xtraplatform = require('@xtraplatform/neutrino');
+const mocha = require('@neutrinojs/mocha');
+
+module.exports = {
+    options: {
+        root: __dirname,
+    },
+    use: [airbnb(), xtraplatform(), mocha()],
+};
