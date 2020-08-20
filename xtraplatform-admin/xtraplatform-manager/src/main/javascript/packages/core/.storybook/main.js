3db1aff4b3da04b017b0eb276a9dfa33c0c17abe
azahnen <zahnen@interactive-instruments.de>
2020-08-20 09:34:19 +0200

    fixes + version bumps

--- xtraplatform-admin/xtraplatform-manager/src/main/javascript/packages/core/.storybook/main.js
+++ xtraplatform-admin/xtraplatform-manager/src/main/javascript/packages/core/.storybook/main.js
@@ -1,5 +1,3 @@
-const util = require('util');
-
 module.exports = {
     stories: [
         '../src/**/*.@(stories|story).mdx',
@@ -13,10 +11,4 @@ module.exports = {
         //    "@storybook/addon-docs",
         '@storybook/addon-essentials',
     ],
-    webpackFinal: (config) => {
-        config.module.rules[0].exclude = /node_modules(?!(\/|\\)@xtraplatform)/;
-        config.module.rules[0].include = '/home/zahnen/development/geo_json';
-        console.log(util.inspect(config, false, null, true));
-        return config;
-    },
 };
