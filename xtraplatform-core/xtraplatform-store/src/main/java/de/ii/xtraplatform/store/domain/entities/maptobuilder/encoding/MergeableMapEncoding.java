41840d2ae3effdd84ce4b52b1813d2e94b74e105
Andreas Zahnen <zahnen@interactive-instruments.de>
2021-04-15 13:23:37 +0200

    immutables improvements (#53)
    * immutables improvements
    
    * formatting

---
+++ xtraplatform-core/xtraplatform-store/src/main/java/de/ii/xtraplatform/store/domain/entities/maptobuilder/encoding/MergeableMapEncoding.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2019-2020 interactive instruments GmbH
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package de.ii.xtraplatform.store.domain.entities.maptobuilder.encoding;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.ImmutableMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.immutables.encode.Encoding;
+
+@Encoding
+class MergeableMapEncoding<T, U> {
+  @Encoding.Impl private Map<T, U> field;
+
+  @Encoding.Expose
+  Map<T, U> getMergeableMap() {
+    return field; // <-- this is how our accessor would be implemented
+  }
+
+  @Encoding.Builder // <-- put annotation
+  static class Builder<T, U> { // <-- copy type parameters from the encoding
+
+    private Map<T, U> mergeableMap = new LinkedHashMap<>();
+
+    @JsonIgnore
+    @Encoding.Init // <-- specify builder initializer method
+    @Encoding.Copy // <-- marks it as "canonical" copy method
+    public void set(Map<T, U> values) {
+      this.mergeableMap = new LinkedHashMap<>();
+      values.forEach(this::put);
+    }
+
+    @JsonProperty
+    @Encoding.Naming(value = "get*")
+    public Map<T, U> get() {
+      return mergeableMap;
+    }
+
+    @Encoding.Init
+    @Encoding.Naming(standard = Encoding.StandardNaming.PUT, depluralize = true)
+    void put(T key, U value) {
+      mergeableMap.put(key, value);
+    }
+
+    @Encoding.Init
+    @Encoding.Naming(standard = Encoding.StandardNaming.PUT_ALL, depluralize = true)
+    void putAll(Map<T, U> values) {
+      values.forEach(this::put);
+    }
+
+    @JsonProperty
+    @Encoding.Init
+    @Encoding.Naming(value = "set*")
+    void putAllJackson(Map<T, U> values) {
+      values.forEach(this::put);
+    }
+
+    @Encoding.Build
+    Map<T, U> build() {
+      return ImmutableMap.copyOf(mergeableMap);
+    }
+  }
+}
