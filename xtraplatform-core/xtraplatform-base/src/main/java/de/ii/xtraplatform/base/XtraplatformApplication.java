988c282ed358cd1e825daa400f717169383c7a21
azahnen <zahnen@interactive-instruments.de>
2022-02-12 11:39:10 +0100

    cleanup

--- xtraplatform-core/xtraplatform-base/src/main/java/de/ii/xtraplatform/base/XtraplatformApplication.java
+++ xtraplatform-core/xtraplatform-base/src/main/java/de/ii/xtraplatform/base/XtraplatformApplication.java
@@ -9,7 +9,6 @@ package de.ii.xtraplatform.base;
 
 import static de.ii.xtraplatform.base.domain.Constants.TMP_DIR_PROP;
 
-import com.google.common.base.Joiner;
 import com.google.common.io.ByteSource;
 import de.ii.xtraplatform.base.domain.ConfigurationReader;
 import de.ii.xtraplatform.base.domain.Constants;
@@ -21,17 +20,15 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /** @author zahnen */
-public class FelixRuntime {
-  private static final Logger LOGGER = LoggerFactory.getLogger(FelixRuntime.class);
+public class XtraplatformApplication {
+  private static final Logger LOGGER = LoggerFactory.getLogger(XtraplatformApplication.class);
 
   private static final String ENV_VAR = "XTRAPLATFORM_ENV";
   private static final String DATA_DIR_NAME = "data";
@@ -43,22 +40,17 @@ public class FelixRuntime {
   private final String version;
   private final Modules modules;
 
-  public FelixRuntime(String name, String version, Modules modules) {
+  public XtraplatformApplication(String name, String version, Modules modules) {
     this.name = name;
     this.version = version;
     this.modules = modules;
   }
 
-  public void init(
-      String[] args,
-      List<ByteSource> baseConfigs) {
-    Map<String, String> felixConfig = new HashMap<>();
+  public void init(String[] args, List<ByteSource> baseConfigs) {
     Path dataDir =
         getDataDir(args).orElseThrow(() -> new IllegalArgumentException("No data directory found"));
-    /*Path bundlesDir =
-        getBundlesDir(args)
-            .orElseThrow(() -> new IllegalArgumentException("No bundles directory found"));*/
     System.setProperty(TMP_DIR_PROP, dataDir.resolve(TMP_DIR_NAME).toAbsolutePath().toString());
+
     Constants.ENV env = parseEnvironment();
     ConfigurationReader configurationReader = new ConfigurationReader(baseConfigs);
     Path configurationFile = configurationReader.getConfigurationFile(dataDir, env);
@@ -70,7 +62,6 @@ public class FelixRuntime {
 
     if (LOGGER.isDebugEnabled()) {
       LOGGER.debug("Data directory: {}", dataDir);
-      //LOGGER.debug("Bundles directory: {}", bundlesDir);
       LOGGER.debug("Environment: {}", env);
       LOGGER.debug("Base configurations: {}", baseConfigs);
     }
@@ -83,45 +74,29 @@ public class FelixRuntime {
         // ignore
       }
     }
-
-    //TODO: to Xtraplatform
-    felixConfig.put(Constants.APPLICATION_KEY, name);
-    felixConfig.put(Constants.VERSION_KEY, version);
-    felixConfig.put(Constants.DATA_DIR_KEY, dataDir.toAbsolutePath().toString());
-    felixConfig.put(Constants.ENV_KEY, env.name());
-    felixConfig.put(Constants.USER_CONFIG_PATH_KEY, configurationFile.toAbsolutePath().toString());
-
-    if (LOGGER.isTraceEnabled()) {
-      LOGGER.trace("Felix config: {}", felixConfig);
-    }
   }
 
   public void start() {
-    try {
-      //felix.start();
-      modules.lifecycle().forEach(Lifecycle::onStart);
-
-      if (LOGGER.isTraceEnabled()) {
-        LOGGER.trace("Felix started");
-      }
-    } catch (Exception ex) {
-      LogContext.error(LOGGER, ex, "Could not start felix");
-    }
+    modules
+        .lifecycle()
+        .forEach(
+            lifecycle -> {
+              try {
+                lifecycle.onStart();
+              } catch (Exception ex) {
+                // TODO: module name
+                LogContext.error(LOGGER, ex, "Module startup error");
+              }
+            });
   }
 
-  public void stop(long timeout) {
+  public void stop() {
     LOGGER.info("Shutting down {}", name);
 
     try {
-      //felix.stop();
-      //felix.waitForStop(timeout);
       modules.lifecycle().forEach(Lifecycle::onStop);
-
-      if (LOGGER.isTraceEnabled()) {
-        LOGGER.trace("Felix stopped");
-      }
     } catch (Exception ex) {
-      LogContext.error(LOGGER, ex, "Could not stop felix");
+      // ignore
     }
 
     LOGGER.info("Stopped {}", name);
@@ -144,24 +119,6 @@ public class FelixRuntime {
     }
 
     return Optional.of(dataDir);
-  }
-
-  private Optional<Path> getBundlesDir(String[] args) {
-    Path bundlesDir;
-
-    if (args.length >= 2) {
-      bundlesDir = Paths.get(args[1]);
-    } else {
-      bundlesDir = Paths.get(BUNDLES_DIR_NAME).toAbsolutePath();
-      if (!Files.isDirectory(bundlesDir)) {
-        bundlesDir = Paths.get("../" + BUNDLES_DIR_NAME).toAbsolutePath();
-      }
-    }
-    if (!Files.isDirectory(bundlesDir)) {
-      return Optional.empty();
-    }
-
-    return Optional.of(bundlesDir);
   }
 
   private Constants.ENV parseEnvironment() {
