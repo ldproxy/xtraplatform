2ae59b43c95040bf5fd4daf11dcbf248ef57baa1
Andreas Zahnen <zahnen@interactive-instruments.de>
2022-05-18 13:25:37 +0200

    restructure (#92)

--- xtraplatform-core/xtraplatform-base/src/main/java/de/ii/xtraplatform/base/domain/LoggingConfiguration.java
+++ xtraplatform-core/xtraplatform-base/src/main/java/de/ii/xtraplatform/base/domain/LoggingConfiguration.java
@@ -10,12 +10,76 @@ package de.ii.xtraplatform.base.domain;
 import com.codahale.metrics.MetricRegistry;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import de.ii.xtraplatform.docs.DocFile;
 import io.dropwizard.logging.DefaultLoggingFactory;
 import io.dropwizard.logging.LoggingUtil;
 
-/** @author zahnen */
-
-/** @title Logging */
+/**
+ * @langEn # Logging
+ * <p>
+ * ## Log-Level
+ * <p>
+ * The log level for the application is `INFO` by default. Other possible
+ *     values are `OFF`, `ERROR` and `WARN`. For debugging it can be set to `DEBUG` for example:
+ *     <p><code>
+ * ```yaml
+ * logging:
+ *   level: DEBUG
+ * ```
+ * </code>
+ *     <p>
+ * ## Log output
+ * <p>
+ * By default, application logs are written to `data/log/xtraplatform.log`.
+ *     Daily log rotation is enabled and old logs are zipped and kept for a week. The log file or
+ *     rotation settings can be changed:
+ *     <p><code>
+ * ```yaml
+ * logging:
+ *   appenders:
+ *     - type: file
+ *       currentLogFilename: /var/log/ldproxy.log
+ *       archive: true
+ *       archivedLogFilenamePattern: /var/log/ldproxy-%d.zip
+ *       archivedFileCount: 30
+ *       timeZone: Europe/Berlin
+ * ```
+ * </code>
+ *     <p>
+ * @langDe # Logging
+ * <p>
+ * ## Log-Level
+ * <p>
+ * Der Log-Level für die Applikation ist standardmäßig `INFO`. Weitere
+ *     mögliche Werte sind `OFF`, `ERROR` und `WARN`. Für die Fehlersuche kann er zum Beispiel auf
+ *     `DEBUG` gesetzt werden:
+ *     <p><code>
+ * ```yaml
+ * logging:
+ *   level: DEBUG
+ * ```
+ * </code>
+ *     <p>
+ * ## Log-Ausgabe
+ * <p>
+ * Standardmäßig werden Applikations-Logs nach `data/log/xtraplatform.log`
+ *     geschrieben. Die tägliche Log-Rotation ist aktiviert und alte Logs werden gezippt und für
+ *     eine Woche verwahrt. Die Log-Datei oder die Rotations-Einstellungen können geändert werden:
+ *     <p><code>
+ * ```yaml
+ * logging:
+ *   appenders:
+ *     - type: file
+ *       currentLogFilename: /var/log/ldproxy.log
+ *       archive: true
+ *       archivedLogFilenamePattern: /var/log/ldproxy-%d.zip
+ *       archivedFileCount: 30
+ *       timeZone: Europe/Berlin
+ * ```
+ * </code>
+ *     <p>
+ */
+@DocFile(path = "application", name = "50-logging.md")
 @JsonTypeInfo(use = JsonTypeInfo.Id.NONE, defaultImpl = LoggingConfiguration.class)
 public class LoggingConfiguration extends DefaultLoggingFactory {
 
