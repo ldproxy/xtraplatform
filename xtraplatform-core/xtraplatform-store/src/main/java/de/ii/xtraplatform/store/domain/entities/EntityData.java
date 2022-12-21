8d03dccac36d550b07184a826f8c1f5d0da2cc87
Andreas Zahnen <zahnen@interactive-instruments.de>
2022-12-21 10:48:53 +0100

    Docs (#121)

--- xtraplatform-core/xtraplatform-store/src/main/java/de/ii/xtraplatform/store/domain/entities/EntityData.java
+++ xtraplatform-core/xtraplatform-store/src/main/java/de/ii/xtraplatform/store/domain/entities/EntityData.java
@@ -8,38 +8,47 @@
 package de.ii.xtraplatform.store.domain.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import de.ii.xtraplatform.docs.DocIgnore;
 import java.time.Instant;
 import java.util.Optional;
 import org.immutables.value.Value;
 
-/**
- * @author zahnen
- */
 public interface EntityData extends de.ii.xtraplatform.store.domain.Value {
 
+  /**
+   * @langEn Unique identifier of the entity, has to match the filename. Allowed characters are
+   *     (A-Z, a-z), numbers (0-9), underscore and hyphen.
+   * @langDe Eindeutiger Identifikator der Entity, muss dem Dateinamen entsprechen. Erlaubt sind
+   *     Buchstaben (A-Z, a-z), Ziffern (0-9), der Unterstrich ("_") und der Bindestrich ("-").
+   */
   String getId();
 
+  @DocIgnore
   @Value.Default
   default long getCreatedAt() {
     return Instant.now().toEpochMilli();
   }
 
+  @DocIgnore
   @Value.Default
   default long getLastModified() {
     return Instant.now().toEpochMilli();
   }
 
+  @DocIgnore
   @Value.Default
   default long getEntityStorageVersion() {
     return 1;
   }
 
+  @DocIgnore
   @JsonIgnore
   @Value.Derived
   default long getEntitySchemaVersion() {
     return 1;
   }
 
+  @DocIgnore
   @JsonIgnore
   Optional<String> getEntitySubType();
 }
