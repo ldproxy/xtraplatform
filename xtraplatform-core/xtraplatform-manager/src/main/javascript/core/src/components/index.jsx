92907f446479ff6e90de65c4b05910cb8acf8140
Andreas Zahnen <zahnen@interactive-instruments.de>
2023-12-16 16:02:10 +0100

    remove manager

--- xtraplatform-core/xtraplatform-manager/src/main/javascript/core/src/components/index.jsx
+++
@@ -1,8 +0,0 @@
-export * from './Layout';
-export * from './Icons';
-export * from './Form';
-export * from './Link';
-export * from './List';
-export { default as InfoLabel } from './InfoLabel';
-export { default as TaskProgress } from './TaskProgress';
-export { default as Async } from './Async';
