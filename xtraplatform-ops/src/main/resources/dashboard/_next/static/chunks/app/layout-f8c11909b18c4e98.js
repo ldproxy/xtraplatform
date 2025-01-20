16ac2b0140d8ac2859466f40919197d610d86cf3
Andreas Zahnen <zahnen@interactive-instruments.de>
2025-01-20 18:50:08 +0100

    fix dashboard

---
+++ xtraplatform-ops/src/main/resources/dashboard/_next/static/chunks/app/layout-f8c11909b18c4e98.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{9383:function(n,e,u){Promise.resolve().then(u.bind(u,2830))}},function(n){n.O(0,[310,606,800,971,69,744],function(){return n(n.s=9383)}),_N_E=n.O()}]);
