79348a84f059851f93073091f3de1b7b3d48ee74
Andreas Zahnen <zahnen@interactive-instruments.de>
2025-07-02 08:46:14 +0200

    improve git store configuration

--- xtraplatform-base/src/main/java/de/ii/xtraplatform/base/domain/StoreSourceGitlab.java
+++ xtraplatform-base/src/main/java/de/ii/xtraplatform/base/domain/StoreSourceGitlab.java
@@ -10,6 +10,7 @@ package de.ii.xtraplatform.base.domain;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.google.common.base.Strings;
 import java.nio.file.Path;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -31,10 +32,11 @@ public interface StoreSourceGitlab extends StoreSourceHttp {
   default StoreSourceGitlab apply() {
     Pattern pattern =
         Pattern.compile(
-            "^(?:([\\w\\-\\.\\/]+)\\/)?([\\w\\-\\.]+)\\/([\\w\\-\\.]+)(?::([\\w\\-\\.]+))?$");
+            "^(?:([\\w\\-\\.\\/:]+?)\\/)?([\\w\\-\\.]+)\\/([\\w\\-\\.]+)(?::([\\w\\-\\.]+))?$");
     Matcher matcher = pattern.matcher(getSrc());
 
     if (matcher.matches()) {
+      String scheme = getInsecure() && Objects.nonNull(matcher.group(1)) ? "http" : "https";
       String host = Optional.ofNullable(matcher.group(1)).orElse("gitlab.com");
       String org = matcher.group(2);
       String repo = matcher.group(3);
@@ -48,7 +50,8 @@ public interface StoreSourceGitlab extends StoreSourceHttp {
       return new ImmutableStoreSourceGitlab.Builder()
           .from(this)
           .src(
-              String.format("https://%s/%s/%s/-/archive/%s/%3$s-%4$s.zip", host, org, repo, branch))
+              String.format(
+                  "%s://%s/%s/%s/-/archive/%s/%3$s-%4$s.zip", scheme, host, org, repo, branch))
           .archiveRoot(String.format("/%s-%s%s", repo, branch, root))
           .label(String.format("%s[%s]", KEY, Path.of(getSrc())))
           .build();
