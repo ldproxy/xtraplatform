cc1fd7652a34bee14f2e1ccedc44a2a3d31d3e3b
Noah <noah.vogt@gmail.com>
2025-09-08 13:07:23 +0200

    added test with null and more value types

--- xtraplatform-cache/src/test/groovy/de/ii/xtraplatform/cache/app/CacheDriverMemSpec.groovy
+++ xtraplatform-cache/src/test/groovy/de/ii/xtraplatform/cache/app/CacheDriverMemSpec.groovy
@@ -8,7 +8,7 @@ import java.sql.ShardingKey
 class CacheDriverMemSpec extends Specification{
 
     @Shared CacheDriverMem cacheDriverMem = new CacheDriverMem()
-    def 'Test put get has'() {
+    def 'Test put get has String'() {
         given:
         String key = "key"
         String value = "Test Object String"
@@ -19,6 +19,84 @@ class CacheDriverMemSpec extends Specification{
         then:
         cacheDriverMem.has(key)
         cacheDriverMem.get(key, String).map(value::equals).orElse(false)
+    }
+
+    def 'Test put get has int'() {
+        given:
+        String key = "key"
+        int value = 11
+
+        when:
+        cacheDriverMem.put(key, value)
+
+        then:
+        cacheDriverMem.has(key)
+        cacheDriverMem.get(key, Integer).map(value::equals).orElse(false)
+    }
+
+    def 'Test put get has double'() {
+        given:
+        String key = "key"
+        double value = 11
+
+        when:
+        cacheDriverMem.put(key, value)
+
+        then:
+        cacheDriverMem.has(key)
+        cacheDriverMem.get(key, Double).map(value::equals).orElse(false)
+    }
+
+    def 'Test put get has boolean'() {
+        given:
+        String key = "key"
+        boolean value = 11
+
+        when:
+        cacheDriverMem.put(key, value)
+
+        then:
+        cacheDriverMem.has(key)
+        cacheDriverMem.get(key, Boolean).map(value::equals).orElse(false)
+    }
+
+    def 'Test put get has List'() {
+        given:
+        String key = "key"
+        List<Integer> value = List.of(10,20,30)
+
+        when:
+        cacheDriverMem.put(key, value)
+
+        then:
+        cacheDriverMem.has(key)
+        cacheDriverMem.get(key, List<Integer>).map(value::equals).orElse(false)
+    }
+
+    def 'Test put get has Map'() {
+        given:
+        String key = "key"
+        Map<Integer, String> value = Map.of(1, "One", 2, "Two")
+
+        when:
+        cacheDriverMem.put(key, value)
+
+        then:
+        cacheDriverMem.has(key)
+        cacheDriverMem.get(key, Map<Integer, String>).map(value::equals).orElse(false)
+    }
+
+    def 'Test put get has int'() {
+        given:
+        String key = "key"
+        int value = 11
+
+        when:
+        cacheDriverMem.put(key, value)
+
+        then:
+        cacheDriverMem.has(key)
+        cacheDriverMem.get(key, Integer).map(value::equals).orElse(false)
     }
 
     def 'Test put get has with validator'() {
@@ -49,7 +127,7 @@ class CacheDriverMemSpec extends Specification{
         cacheDriverMem.get(key, String).map(value::equals).orElse(false)
     }
 
-    def 'Test put get has with ttl and validator'() {
+    def 'Test put get has with ttl and validator STRING'() {
         given:
         String key = "key"
         String validator = "validator"
@@ -64,6 +142,7 @@ class CacheDriverMemSpec extends Specification{
         cacheDriverMem.get(key, validator, String).map(value::equals).orElse(false)
     }
 
+
     def 'Test delete'() {
         given:
         String key = "key"
@@ -76,5 +155,56 @@ class CacheDriverMemSpec extends Specification{
         then:
         !cacheDriverMem.has(key)
         !cacheDriverMem.get(key, String).isPresent()
+    }
+
+    def 'Test empty String key'() {
+        given:
+        String key = ""
+        String value = "Test Object String"
+
+        when:
+        cacheDriverMem.put(key, value)
+
+        then:
+        cacheDriverMem.has(key)
+        cacheDriverMem.get(key, String).map(value::equals).orElse(false)
+    }
+
+    def 'Test empty String value'() {
+        given:
+        String key = "key"
+        String value = ""
+
+        when:
+        cacheDriverMem.put(key, value)
+
+        then:
+        cacheDriverMem.has(key)
+        cacheDriverMem.get(key, String).map(value::equals).orElse(false)
+    }
+
+    def 'Test null key'() {
+        given:
+        String key = null
+        String value = "Test Object String"
+
+        when:
+        cacheDriverMem.put(key, value)
+
+        then:
+        thrown(NullPointerException)
+    }
+
+    def 'Test null value'() {
+        given:
+        String key = "key"
+        String value = null
+
+        when:
+        cacheDriverMem.put(key, value)
+
+        then:
+        cacheDriverMem.has(key)
+        cacheDriverMem.get(key, String).isEmpty()
     }
 }
