38e3ea80aaaa8822880ca314e0c15819480bdeda
Noah <noah.vogt@gmail.com>
2025-09-12 14:55:51 +0200

    more tests for JobQueueSimple

--- xtraplatform-jobs/src/test/groovy/de/ii/xtraplatform/jobs/app/JobQueueSimpleSpec.groovy
+++ xtraplatform-jobs/src/test/groovy/de/ii/xtraplatform/jobs/app/JobQueueSimpleSpec.groovy
@@ -57,24 +57,23 @@ class JobQueueSimpleSpec extends Specification{
 
     def 'Test push'() {
         given:
-        Job job = Job.of("foo", 5, "bar")
+        Job job = Job.of("push", 5, "bar")
 
         when:
         jobQueueSimple.push(job, false)
 
-
         then:
-        job == jobQueueSimple.getOpen().get("foo").get(5)[0]
+        job == jobQueueSimple.getOpen().get("push").get(5)[0]
 
 
     }
     def 'Test take'() {
         given:
-        Job job = Job.of("foo", 5, "bar")
+        Job job = Job.of("take", 5, "bar")
 
         when:
         jobQueueSimple.push(job, false)
-        Job jobTaken = jobQueueSimple.take("foo", "").get()
+        Job jobTaken = jobQueueSimple.take("take", "").get()
 
         then:
         jobTaken.equals(jobQueueSimple.getTaken()[0])
@@ -112,5 +111,112 @@ class JobQueueSimpleSpec extends Specification{
         then:
         !jobQueueSimple.done(job.id)
     }
+
+    def 'Test push JobSet'() {
+        given:
+        JobSet jobSet= JobSet.of("jobSet", 5, "entity", "label", "description", new JobSet.JobSetDetails() {
+            @Override
+            void update(Map<String, String> parameters) {
+
+            }
+
+            @Override
+            void reset(Job job) {
+
+            }
+
+            @Override
+            String getLabel() {
+                return null
+            }
+        })
+
+        when:
+        jobQueueSimple.push(jobSet, false)
+
+        then:
+        jobSet == jobQueueSimple.getSets()[0]
+    }
+
+    def 'Test done JobSet'() {
+        given:
+        JobSet jobSet= JobSet.of("doneSet", 5, "entity", "label", "description", new JobSet.JobSetDetails() {
+            @Override
+            void update(Map<String, String> parameters) {
+
+            }
+
+            @Override
+            void reset(Job job) {
+
+            }
+
+            @Override
+            String getLabel() {
+                return null
+            }
+        })
+
+        when:
+        jobQueueSimple.push(jobSet, false)
+
+        then:
+        jobQueueSimple.doneSet(jobSet.id)
+    }
+
+    def 'Test done JobSet 2'() {
+        given:
+        JobSet jobSet= JobSet.of("doneSet2", 5, "entity", "label", "description", new JobSet.JobSetDetails() {
+            @Override
+            void update(Map<String, String> parameters) {
+
+            }
+
+            @Override
+            void reset(Job job) {
+
+            }
+
+            @Override
+            String getLabel() {
+                return null
+            }
+        })
+
+        when:
+        jobQueueSimple.push(jobSet, false)
+        jobQueueSimple.doneSet(jobSet.id)
+
+        then:
+        !jobQueueSimple.doneSet(jobSet.id)
+    }
+
+    def 'Test push null'() {
+        given:
+        Job job = null
+
+        when:
+        jobQueueSimple.push(job, false)
+
+        then:
+        thrown(NullPointerException)
+
+
+    }
+
+    def 'Test push empty strings'() {
+        given:
+        Job job = Job.of("", 5, "")
+
+        when:
+        jobQueueSimple.push(job, false)
+
+        then:
+        job == jobQueueSimple.getOpen().get("").get(5)[0]
+
+
+    }
+
+
 
 }
