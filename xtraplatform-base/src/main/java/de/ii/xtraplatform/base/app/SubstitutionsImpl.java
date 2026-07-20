b531339c3d30020b5b9101460bc8de866b592798
Andreas Zahnen <zahnen@interactive-instruments.de>
2026-07-20 16:33:52 +0200

    introduce substitution transformers

--- xtraplatform-base/src/main/java/de/ii/xtraplatform/base/app/SubstitutionsImpl.java
+++ xtraplatform-base/src/main/java/de/ii/xtraplatform/base/app/SubstitutionsImpl.java
@@ -8,15 +8,19 @@
 package de.ii.xtraplatform.base.app;
 
 import com.github.azahnen.dagger.annotations.AutoBind;
+import com.google.common.base.Strings;
 import de.ii.xtraplatform.base.domain.AppContext;
 import de.ii.xtraplatform.base.domain.Substitutions;
 import io.dropwizard.configuration.UndefinedEnvironmentVariableException;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Objects;
 import org.apache.commons.text.StringSubstitutor;
 import org.apache.commons.text.TextStringBuilder;
@@ -28,6 +32,7 @@ import org.slf4j.LoggerFactory;
 public class SubstitutionsImpl implements Substitutions {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SubstitutionsImpl.class);
+  private static final String TRANSFORMER_BASE64 = "base64";
 
   private final Map<String, String> constants;
 
@@ -43,16 +48,23 @@ public class SubstitutionsImpl implements Substitutions {
 
   private String lookup(String key) {
     String envKey = key.replaceAll("\\.", "_");
+    String transformer = "";
+
+    if (envKey.contains(">")) {
+      transformer = envKey.substring(envKey.indexOf('>') + 1);
+      envKey = envKey.substring(0, envKey.indexOf('>'));
+    }
+
     String value = System.getenv(envKey);
 
     if (Objects.nonNull(value)) {
-      return value;
+      return transform(value, transformer);
     }
 
     value = System.getenv(envKey.toUpperCase(Locale.ROOT));
 
     if (Objects.nonNull(value)) {
-      return value;
+      return transform(value, transformer);
     }
 
     if (constants.containsKey(key)) {
@@ -60,6 +72,18 @@ public class SubstitutionsImpl implements Substitutions {
     }
 
     return null;
+  }
+
+  private static String transform(String value, String transformer) {
+    if (Strings.isNullOrEmpty(transformer)) {
+      return value;
+    }
+
+    if (TRANSFORMER_BASE64.equals(transformer)) {
+      return Base64.getEncoder().encodeToString(value.getBytes(StandardCharsets.UTF_8));
+    }
+
+    throw new IllegalArgumentException("Unknown substitution transformer: " + transformer);
   }
 
   public class VariableSubstitutor extends StringSubstitutor {
@@ -90,7 +114,7 @@ public class SubstitutionsImpl implements Substitutions {
   private static Map<String, String> extract(Map<String, Object> constants, String prefix) {
     Map<String, String> result = new LinkedHashMap<>();
 
-    for (Map.Entry<String, Object> entry : constants.entrySet()) {
+    for (Entry<String, Object> entry : constants.entrySet()) {
       if (entry.getValue() instanceof Map) {
         result.putAll(
             extract((Map<String, Object>) entry.getValue(), prefix + entry.getKey() + "."));
